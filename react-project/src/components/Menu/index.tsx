cbbb4da8ce4c8578ef5c45e9bce665be61683e31
victsilva37 <victormau.sm@gmail.com>
2025-03-31 19:03:52 -0300

    Se agrega el componente del menú

--- react-project/src/components/Menu/index.tsx
+++ react-project/src/components/Menu/index.tsx
@@ -4,11 +4,10 @@ function Menu(){
     return(
         <div>
             <div id="menu-content">
-                <h1>BLANQUITA MELIPILLA</h1>
+                <h1>💙🤍BLANQUITA MELIPILLA💙🤍</h1>
                 <nav>
                     <ul>
                         <li><a href="#inicio">Inicio</a></li>
-                        <li><a href="#nosotros">Nosotros</a></li>
                         <li><a href="#productos">Productos</a></li>
                         <li><a href="#contacto">Contacto</a></li>
                     </ul>
