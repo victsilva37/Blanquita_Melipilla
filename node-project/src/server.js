4ff93b6b7da4cfdcf8a2f27d079617d104bf61af
victsilva37 <victormau.sm@gmail.com>
2025-09-05 15:29:56 -0400

    Se intenta arreglar las rutas

--- node-project/src/server.js
+++ node-project/src/server.js
@@ -1,32 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
+// CORS
+app.use(cors({ origin: "*" }));
 
-// O si quieres permitir solo tu frontend:
-app.use(cors({
-  origin: "*"
-}));
+// Middlewares
+app.use(express.json());
 
+// Rutas API
 const productoRoutes = require("./endpoints/producto");
 const authRoutes = require("./endpoints/login_auth");
-
-app.use(express.json());
 app.use("/api/productos", productoRoutes);
 app.use("/api", authRoutes);
 
-const path = require("path");
+// Servir frontend
+app.use(express.static(path.join(__dirname, "dist"))); // 👈 tu carpeta compilada de React
 
-// Middleware para servir el frontend compilado
-app.use(express.static(path.join(__dirname, "dist")));
-
-// Catch-all: para cualquier ruta que no sea /api
-app.get("*", (req, res) => {
+// Catch-all para React Router
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-
+// Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
