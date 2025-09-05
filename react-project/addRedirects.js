1a9da7d56bb6b652eaf3960b6b819bf8c8e466eb
victsilva37 <victormau.sm@gmail.com>
2025-09-05 16:56:07 -0400

    aaa

--- react-project/addRedirects.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-// Ruta al dist
-const distPath = path.join(__dirname, "dist", "_redirects");
-
-fs.writeFileSync(distPath, "/* /index.html 200");
-console.log("_redirects file created in dist!");
